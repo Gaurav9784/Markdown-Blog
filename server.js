9747030c9eae3d345a8b1e51457425937263b510
Great9784 <gs6511916@gmail.com>
2020-07-16 22:23:25 +0530

    Adding projects files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Article =require("./models/articles");
+const methodOverride = require("method-override");
+const articleRouter = require('./routes/articles');
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog',{ useNewUrlParser: true , useUnifiedTopology: true ,useCreateIndex: true});
+
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.get('/',async (req,res) =>{
+	const articles =await Article.find().sort({ date: 'desc'});
+	res.render('./article/index',{articles: articles});
+});
+app.use('/articles',articleRouter);
+app.listen(5000);
